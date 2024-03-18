3919dc4d584831918230ae7e0f5c59c2747fbff8
Sowmiya <114464938+Sowmiya214@users.noreply.github.com>
2024-03-18 09:21:44 +0530

    Add files via upload

---
+++ ProdReportMapRepository.java
@@ -0,0 +1,9 @@
+package com.jsw.fe.transaction.Repository;
+
+import com.jsw.fe.transaction.Entity.ProdMapMstrModel;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProdReportMapRepository extends PagingAndSortingRepository<ProdMapMstrModel, Long> {
+
+
+}
